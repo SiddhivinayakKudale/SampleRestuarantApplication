12e0a636f2d58e492e7562e99e3ee4dde4d958e2
sagar <sagarkudale2014.sk@gmail.com>
2023-07-21 15:05:27 +0800

    Add project files.

---
+++ SampleRestuarantApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleRestuarantApplication.Startup))]
+namespace SampleRestuarantApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
